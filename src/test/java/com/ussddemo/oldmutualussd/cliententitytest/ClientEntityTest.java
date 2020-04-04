b0b2480917436a16e35efdf10ec09211834dfff0
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-04 20:50:19 +0100

    testing

--- src/test/java/com/ussddemo/oldmutualussd/cliententitytest/ClientEntityTest.java
+++ src/test/java/com/ussddemo/oldmutualussd/cliententitytest/ClientEntityTest.java
@@ -4,16 +4,11 @@ package com.ussddemo.oldmutualussd.cliententitytest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.jboss.logging.Logger;
-import org.junit.Before;
+import java.util.logging.Logger;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.ussddemo.oldmutualussd.UssdappApplication;
 import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
 
 @SpringBootTest
