abe78a91260b3afc37487499938290437b9711ae
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-04 20:58:29 +0100

    test

--- src/test/java/com/ussddemo/oldmutualussd/cliententitytest/ClientEntityTest.java
+++ src/test/java/com/ussddemo/oldmutualussd/cliententitytest/ClientEntityTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.logging.Logger;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
