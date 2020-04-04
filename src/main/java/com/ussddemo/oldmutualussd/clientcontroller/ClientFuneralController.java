d9d6dfb6df44dde3f54d89b5723792a33bb391a6
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-04 21:38:18 +0100

    test

---
+++ src/main/java/com/ussddemo/oldmutualussd/clientcontroller/ClientFuneralController.java
@@ -0,0 +1,30 @@
+package com.ussddemo.oldmutualussd.clientcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
+
+
+@RestController
+public class ClientFuneralController {
+
+	
+	@Autowired
+	private ClientEntity myClient;
+	
+	
+	
+	@GetMapping(path="/")
+    public ClientEntity getMyClient(){
+		
+		
+    
+        return myClient;
+	
+    }
+	
+	
+	
+}
