2c830de16f55901de6b269aa459e885a023f2cf1
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-04 21:48:13 +0100

    test

--- src/main/java/com/ussddemo/oldmutualussd/UssdappApplication.java
+++ src/main/java/com/ussddemo/oldmutualussd/UssdappApplication.java
@@ -2,10 +2,6 @@ package com.ussddemo.oldmutualussd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
 
 @SpringBootApplication
 //@ComponentScan(basePackageClasses = { ClientEntity.class})
@@ -13,7 +9,7 @@ public class UssdappApplication {
 
 	public static void main(String[] args) {
 		
-		 new SpringApplicationBuilder(UssdappApplication.class).run();
+		SpringApplication.run(UssdappApplication.class, args);
     
 //		SpringApplication.run(UssdappApplication.class, args);
 	}
