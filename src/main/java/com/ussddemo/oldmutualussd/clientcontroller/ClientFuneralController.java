5ed2f46dbff28a5552572c2b3aee2cf871438fbe
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-05 07:48:57 +0100

    test parameter

--- src/main/java/com/ussddemo/oldmutualussd/clientcontroller/ClientFuneralController.java
+++ src/main/java/com/ussddemo/oldmutualussd/clientcontroller/ClientFuneralController.java
@@ -2,12 +2,14 @@ package com.ussddemo.oldmutualussd.clientcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
 
 
 @RestController
+@RequestMapping
 public class ClientFuneralController {
 
 	
