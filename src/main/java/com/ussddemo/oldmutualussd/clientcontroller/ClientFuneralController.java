9e21e7fc1004fdebb0609fab08eaecc400306219
oyeyipoadefunmi90@gamil.com <oyeyipoadefunmi90@gamil.com>
2020-04-05 10:56:26 +0100

    test parameter

--- src/main/java/com/ussddemo/oldmutualussd/clientcontroller/ClientFuneralController.java
+++ src/main/java/com/ussddemo/oldmutualussd/clientcontroller/ClientFuneralController.java
@@ -1,16 +1,19 @@
 package com.ussddemo.oldmutualussd.clientcontroller;
 
 
+import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ussddemo.oldmutualussd.funeralussd.ClientEntity;
 
 
 @RestController
-@RequestMapping
+
 public class ClientFuneralController {
 
 	
@@ -19,7 +22,9 @@ public class ClientFuneralController {
 	
 	
 	
-	@GetMapping(path="/")
+	@RequestMapping(value = "/",
+	        method = RequestMethod.GET)
+	@ResponseBody
     public ClientEntity getMyClient(){
 		
 		
